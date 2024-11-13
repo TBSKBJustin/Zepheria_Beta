f8161948bcc31542d7a0e0063553070a57e82e5a
Susana016 <shernand850@gmail.com>
2024-11-13 11:16:38 -0700

    aded menu button

---
+++ Assets/Scripts/ToggleHandMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToggleHandMenu : MonoBehaviour
+{
+    public InputActionReference menuButtonAction; // Link your custom action here
+    public GameObject handMenuCanvas;
+
+    private void OnEnable()
+    {
+        menuButtonAction.action.performed += ToggleMenuVisibility;
+    }
+
+    private void OnDisable()
+    {
+        menuButtonAction.action.performed -= ToggleMenuVisibility;
+    }
+
+    private void ToggleMenuVisibility(InputAction.CallbackContext context)
+    {
+        handMenuCanvas.SetActive(!handMenuCanvas.activeSelf);
+    }
+}
